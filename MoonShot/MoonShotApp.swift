bc818b1ee80c5d3ca49a64528d80ec14f73b2be0
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-28 19:09:54 +0300

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Николай Никитин on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
